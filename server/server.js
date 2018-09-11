4cc8867ccae5ea1c6e58a5d75b2590b35ab6dae3
Mukherjee <apurvam@mgc.mentorg.com>
2018-09-11 02:26:03 -0700

    Add create and new message event handlers

--- server/server.js
+++ server/server.js
@@ -14,6 +14,26 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected!');
 
+    // socket.emit('newEmail', {
+    //     from: 'andrew@example.com',
+    //     text: 'Hey. Welcome back',
+    //     createdAt: 122323
+    // });
+
+    // socket.on('createEmail', (email) => {
+    //     console.log('createEmail', email);
+    // });
+
+    socket.emit('newMessage', {
+        from: 'Andrew',
+        text: 'Let us meet up at 6 to discuss the updates',
+        createdAt: 353365
+    });
+
+    socket.on('createMessage', (message) => {
+        console.log('createMessage', message);
+    })
+
     socket.on('disconnect', () => {
         console.log('Disconnected from client');
     });
